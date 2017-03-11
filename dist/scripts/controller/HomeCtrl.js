bf66b001c5949d06c53e2bf3f77dcc120cb68ce4
Christopher Bazin <cjbazin@gmail.com>
2017-03-11 14:18:07 -0800

    refactored but need to get default room

--- dist/scripts/controller/HomeCtrl.js
+++ dist/scripts/controller/HomeCtrl.js
@@ -12,51 +12,19 @@
     /* Get the current Chat User Name */
     $scope.currentChatUser = $cookies.get('blocChatCurrentUser');
 
-
-    /* What room is the current room */
-    if ($scope.selectRoom === undefined){
-      // need to define a default state
-       $scope.currentRoom = "";
-    }
-
     /* When someone in the view selects a room, run this function */
-    $scope.selectRow = function(name,roomId){
+    $scope.selectRoom = function(name,roomId){
       $scope.currentRoom = name;
       $scope.currentRoomId = roomId;
       $scope.roomMessages = Message.getByRoomId($scope.currentRoomId);
     }
 
     this.submitMessage = function(newMessage){
-      var d = new Date();
-      var messageStamp = formatTime(d); //just get the time stamp
-      
-      //check to make sure there is a room for people to chat in
-      if (!$scope.currentRoom) {
-        alert('You need to select a room to chat in!');
-        errorFlag = true;
-      }
-
-      
-      if ((newMessage !== undefined)) {
-        Message.send(newMessage,$scope.currentChatUser,$scope.currentRoomId,messageStamp);
-      }
-
-
-
+      Message.send(newMessage.message,$scope.currentRoomId,$scope.currentChatUser); 
+      this.messageToSend={};
     }
-
   }
 
-  function formatTime(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var amORpm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; 
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + amORpm;
-    return strTime;
-  }
 
  angular
     .module('blocChat')
